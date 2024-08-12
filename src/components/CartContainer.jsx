055d3c90bf0e23bc0cf81dd74a79c9c9a06b47c5
Gyanendra <gyanen56@gmail.com>
2024-08-12 14:17:52 +1000

    displaying cart items

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -4,10 +4,8 @@ import CartItem from "./CartItem"
 const CartContainer = () => {
    const {amount, cartItems,} =  useSelector((store)=>store.cart)
   console.log(amount)
-  if(amount <1
-
-  ){
-    return <div className="container mt-4">
+  if(amount <1){
+    return <div className="container mt-5">
     <h2 className="text-center">Your bag</h2>
     <div className="text-center">
         is Empty
@@ -15,11 +13,21 @@ const CartContainer = () => {
     </div>
   }
   return (
-    <div className="container mt-3">
-        <h2 className="text-center"> Your bag</h2>
-        {cartItems.map((item)=>{
-            return <CartItem key={item.id} {...item}/>
-        })}
+    <div className="container mt-5">
+      <h2 className="text-center"> Your bag</h2>
+      {cartItems.map((item) => {
+        return <CartItem key={item.id} {...item} />
+      })}
+      <footer>
+        <hr />
+        <div className="d-flex justify-content-between">
+          <div className="fs-4">Total </div>
+          <div className="fs-4">${amount.toFixed(2)}</div>
+        </div>
+        <div className="text-center mt-1 ">
+            <button className="btn btn-outline-danger px-4 py-2 border border-2 border-danger fs-5  ">Clear Cart</button>
+        </div>
+      </footer>
     </div>
   )
 }
--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,6 +1,25 @@
-const CartItem = ({}) => {
+import { BsArrowUpCircleFill } from 'react-icons/bs'
+import { BsArrowDownCircleFill } from 'react-icons/bs'
+
+const CartItem = ({ id, img, title, price, amount }) => {
   return (
-    <div>CartItem</div>
+    <div className="my-1">
+      <div className="row align-items-center  ">
+        <div className="col-10 d-flex align-items-center ">
+          <img src={img} alt={title} style={{ height: '150px' }} />
+          <div className="text-capitalize">
+            <h4>{title}</h4>
+            <p className="fs-5">${price}</p>
+            <button className="btn btn-danger">remove</button>
+          </div>
+        </div>
+        <div className="col-2 text-center">
+          <BsArrowDownCircleFill className="icons fs-3 text-success " />
+          <div className="fs-4">{amount}</div>
+          <BsArrowUpCircleFill className="icons fs-3 text-success" />
+        </div>
+      </div>
+    </div>
   )
 }
 export default CartItem
